8652d661f2edac461cd6aa40b017c20bd5e9d330
Azat Khalilov <azat_khalilov@epam.com>
2021-11-23 14:12:34 +0500

    feat(backend): add entity for simple table and make logic retrieving data from database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {MedicalEntity} from "./entities/medical.entity";
+import {getConnectionOptions} from "typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+        }),
+    }),
+    TypeOrmModule.forFeature([MedicalEntity])
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {MedicalEntity} from "./entities/medical.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@InjectRepository(MedicalEntity) public  medicalRepository:Repository<MedicalEntity>) {
+  }
+  getSimpleTable():Promise<MedicalEntity[]> {
+    return this.medicalRepository.find();
   }
 }
